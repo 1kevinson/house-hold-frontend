8013d00ffb4c90b98215c9e8eed3a5d37b088ca2
Arsene Kevin <kevinkouomeu@icloud.com>
2020-09-08 16:26:02 +0200

    Added based components

--- web-household/src/app/app.module.ts
+++ web-household/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { ProfileBoxComponent } from './main/profile-box/profile-box.component';
+import { NavLinksComponent } from './main/nav-links/nav-links.component';
+import { MainViewComponent } from './main/main-view/main-view.component';
+import { HomeComponent } from './main/main-view/home/home.component';
+import { BillsComponent } from './main/main-view/bills/bills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    ProfileBoxComponent,
+    NavLinksComponent,
+    MainViewComponent,
+    HomeComponent,
+    BillsComponent
   ],
   imports: [
     BrowserModule
