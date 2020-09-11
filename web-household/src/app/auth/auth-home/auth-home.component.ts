72c97ddf911e34f47836c6c3d5d8ea6cf06ae7a4
Arsene Kevin <kevinkouomeu@icloud.com>
2020-09-11 02:13:06 +0200

    ⚙️ - Handling controls on login component

--- web-household/src/app/auth/auth-home/auth-home.component.ts
+++ web-household/src/app/auth/auth-home/auth-home.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-home',
   templateUrl: './auth-home.component.html',
-  styleUrls: ['./auth-home.component.css']
+  styleUrls: ['./auth-home.component.css'],
 })
 export class AuthHomeComponent implements OnInit {
+  // Accesing the form datas with view Child
+  @ViewChild('loginForm') loginForm: NgForm;
 
-  constructor() { }
+  //Property binding
+  emailLoginProperty: string;
+  passwordLoginProperty: string;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmitLogin() {
+    console.log(this.loginForm);
   }
 
+  onCancelLogin() {
+    this.emailLoginProperty = '';
+    this.passwordLoginProperty = '';
+  }
 }
